83af4e1390d83b5eb54fda922f1d7bb064b8c4d0
Petrov Roman <105771480+RVPetrov2022@users.noreply.github.com>
2026-08-06 09:19:32 +0300

    Update Readme.cpp

--- Readme.cpp
+++ Readme.cpp
@@ -1,4 +1,4 @@
 * [Обзор итоговый USDX USDR на Yotube ](https://youtu.be/iIhIMlRWpWc?si=LlxvQUvjvHFxNpIo)
 * [Обзор  USDX USDR V2 на Yotube (Часть 1)](https://youtu.be/HYh8lDrk8nc?si=IW0tmxWdyMkNyXpr)
 * [Обзор  USDX USDR V2 на Yotube (Часть 2)](https://youtu.be/gQgePV4C6zQ?si=P2BdlA88EICBvza_)
-* [Обзор USDX USDR V2 на Yotube (Часть 3)](https://youtu.be/Hl7mBRa86f8?si=FFlUVe0X0l5oPk3N
+* [Обзор USDX USDR V2 на Yotube (Часть 3)](https://youtu.be/Hl7mBRa86f8?si=FFlUVe0X0l5oPk3N)
